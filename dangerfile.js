076ebc174d385c0015fdb8ef6334605480b7b566
Arun Shankar <ashankar@tubi.tv>
2022-02-09 22:06:25 -0800

    Check

--- dangerfile.js
+++ dangerfile.js
@@ -1,17 +1,39 @@
-import {message, danger} from "danger"
-
-const modifiedMD = danger.git.modified_files.join("- ")
-console.log('danger.git.modified_files', danger.git.modified_files)
-console.log('danger.git.created_files', danger.git.created_files)
-console.log('danger.git.deleted_files', danger.git.deleted_files)
+import { message, danger, warn } from "danger"
 
 const modifiedFiles = danger.git.modified_files;
 const createdFiles = danger.git.created_files;
-const deletedFiles = danger.git.deleted_files;
-const changedFiles = [...modifiedFiles, ...createdFiles, ...deletedFiles];
-const promises = changedFiles.map(fileName => danger.git.diffForFile(fileName))
-Promise.all(promises)
-  .then((data) => {
-    console.log(data)
-  })
-message("Changed Files in this PR: \n - " + modifiedMD)
+const changedFiles = [...modifiedFiles, ...createdFiles];
+
+console.log('** changedFiles **', changedFiles)
+
+const filePattern = 'coverage'
+const codeBlock = 'logExposure'
+
+let filesChanged = false;
+for (let file of changedFiles) {
+  if (file.indexOf(filePattern) > -1) {
+    filesChanged = true;
+    break;
+  }
+}
+
+if (filesChanged) {
+  const promises = changedFiles.map(fileName => danger.git.diffForFile(fileName))
+  Promise.all(promises)
+    .then((filesChanged) => {
+      let isCodeBlockAdded = false;
+      filesChanged.forEach(fChange => {
+        if (fChange.added.indexOf(codeBlock) > -1) {
+          isCodeBlockAdded = true;
+        }
+      })
+
+      if (!isCodeBlockAdded) {
+        warn(`Looks like ${codeBlock} is not added. Please make sure you add it.`)
+      } else {
+        message(`Good job! Your PR has ${codeBlock}`)
+      }
+    })
+} else {
+  message(`Looks like files matching pattern ${filePattern} were not changed`)
+}
