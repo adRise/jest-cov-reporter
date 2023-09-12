35f0b797cc4e41a0f3af9baf6564a07033de2db3
islyn <liuying@tubi.tv>
2023-09-12 21:09:58 +0800

    build

--- dest/index.js
+++ dest/index.js
@@ -15552,31 +15552,48 @@ const unpackage = (packages) => {
       });
     });
 
+    let lineStart = Infinity;
+    let lineEnd = 0;
     c.lines && c.lines[0].line && c.lines[0].line.forEach((l) => {
       coverageSummary[c.$.name].statements.total ++;
       if (l.$.hits === '1') {
         coverageSummary[c.$.name].statements.covered ++;
+      } else {
+        coverageSummary[c.$.name].statements.skipped ++;
       }
 
       if (l.$.branch === 'true') {
         coverageSummary[c.$.name].branches.total ++;
         if (l.$.hits === '1') {
           coverageSummary[c.$.name].branches.covered ++;
+        } else {
+          coverageSummary[c.$.name].branches.skipped ++;
         }
       }
+
+      if (lineStart > Number(l.$.number)) lineStart = Number(l.$.number);
+      if (lineEnd < Number(l.$.number)) lineEnd = Number(l.$.number);
     });
+
+    coverageSummary[c.$.name].lines.total = lineEnd - lineStart + 1;
+    coverageSummary[c.$.name].lines.skipped = coverageSummary[c.$.name].statements.skipped;
+    coverageSummary[c.$.name].lines.covered = coverageSummary[c.$.name].lines.total - coverageSummary[c.$.name].statements.skipped;
+
     c.methods && c.methods[0].method && c.methods[0].method.forEach((m) => {
       coverageSummary[c.$.name].functions.total ++;
-      if (m.$.hits === '1') {
+      if (Number(m.$['line-rate']) + Number(m.$['branch-rate']) > 0) {
         coverageSummary[c.$.name].functions.covered ++;
+      } else {
+        coverageSummary[c.$.name].functions.skipped ++;
       }
     })
+
     coverageType.forEach(type => {
       coverageDetails.forEach((detail) => {
         coverageSummary.total[type][detail] += coverageSummary[c.$.name][type][detail];
       });
-      coverageSummary.total[type].pct = percentage(coverageSummary.total[type].covered, coverageSummary.total[type].total);
       coverageSummary[c.$.name][type].pct = percentage(coverageSummary[c.$.name][type].covered, coverageSummary[c.$.name][type].total);
+      coverageSummary.total[type].pct = percentage(coverageSummary.total[type].covered, coverageSummary.total[type].total);
     });
   });
 
