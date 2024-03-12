65fa094ffca3f6ac0839d5d86364ba39000efb1c
Liuying Zhang <liuying@tubi.tv>
2024-03-12 21:15:07 +0800

    chore

--- src/DiffChecker.js
+++ src/DiffChecker.js
@@ -40,6 +40,7 @@ export class DiffChecker {
     for (const filePath of reportKeys) {
       const newCoverage = coverageReportNew[filePath] || {};
       const oldCoverage = coverageReportOld[filePath] || {};
+      console.log(filePath)
       this.diffCoverageReport[filePath] = {
         branches: {
           new: newCoverage.branches,
