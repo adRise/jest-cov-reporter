d4e5e87c529611805608267378fa0ce2b2371335
Arun Shankar <ashankar@tubi.tv>
2025-06-01 22:14:25 -0700

    chore: remove js files

--- src/legacy.index.js
+++
@@ -1,5 +0,0 @@
-// This file is a wrapper that imports from the compiled TypeScript version
-// All functionality is now maintained in index.ts
-
-// Simply re-export everything from the compiled output
-module.exports = require('../lib/index');
